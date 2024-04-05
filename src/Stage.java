e2bd66e7f24af16c5b6d4aa4155bdb1e41ad54bc
hakie2kim <115719016+hakie2kim@users.noreply.github.com>
2024-04-05 18:03:48 +0900

    fix: 기존 레포 삭제 후 옮김

--- src/Stage.java
+++ src/Stage.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
 
 public class Stage {
-  private int quantity;
+    private int quantity;
 
-  public void start() {
-    Scanner sc = new Scanner(System.in);
-		System.out.println("사용량을 입력하세요.");
-    quantity = sc.nextInt();
+    public void start() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("사용량을 입력하세요.>");
+        quantity = sc.nextInt();
 
-    // code here
-  }
+        // code here
+        Calculation calculation = Calculation.getInstance(quantity);
+        System.out.println(calculation);
+    }
 }
